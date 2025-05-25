1c51297f0b4a307e65ca05cf3f0ad2f73663399d
liferreirab <li.ferreirab@gmail.com>
2025-05-25 21:13:37 +0000

    changin location of pixel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { useEffect } from 'react';
+import ReactPixel from 'react-facebook-pixel';
 
 const queryClient = new QueryClient();
+const PIXEL_ID = '398220617905716';
+
+const PixelTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactPixel.init(PIXEL_ID);
+    ReactPixel.pageView(); // Initial page load
+  }, []);
+
+  useEffect(() => {
+    ReactPixel.pageView(); // On route change
+  }, [location]);
+
+  return null;
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -14,6 +32,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <PixelTracker />
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
